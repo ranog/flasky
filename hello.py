ea362e64a9cd1c6fc8c7e9d30438d4622ed47f09
ranog <jprnogueira@yahoo.com.br>
2021-07-04 17:08:12 -0300

    Ex 3-6: custom error pages

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,3 +13,13 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
