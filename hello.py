85a01ccefdf8f98570a3fe0299f21a9f624b27d7
ranog <jprnogueira@yahoo.com.br>
2021-07-06 22:36:44 -0300

    Ex 5-1: database configuration

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class NameForm(FlaskForm):
@@ -12,10 +16,15 @@ class NameForm(FlaskForm):
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+    basedir,
+    'data.sqlite'
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
