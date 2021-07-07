8b1a15aedfa5b3e1dcc442ddbf305d64f4fa8b4f
ranog <jprnogueira@yahoo.com.br>
2021-07-06 21:53:13 -0300

    Ex 4-5: redirects and user sessions

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -20,12 +19,11 @@ moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
