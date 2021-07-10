ddcd378b66c66c560ee70016825eff2dee90f3f5
ranog <jprnogueira@yahoo.com.br>
2021-07-10 19:06:50 -0300

    Ex 5-7: adding a shell context

--- hello.py
+++ hello.py
@@ -83,3 +83,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
