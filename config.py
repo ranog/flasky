fc84001a1eda4cbaba0ec1e10ff2fca1222e3da0
ranog <jprnogueira@yahoo.com.br>
2021-07-13 21:05:05 -0300

    Ex 7-3: application package constructor

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'hard to guess string')
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     MAIL_PORT = os.environ.get('MAIL_PORT', '587')
     MAIL_USE_TLS = (
