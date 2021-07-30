b33a256557ad8efd109efe599c785edda1341303
ranog <jprnogueira@yahoo.com.br>
2021-07-30 08:53:09 -0300

    Ex 8-23: resending the account confirmation email

--- app/auth/views.py
+++ app/auth/views.py
@@ -90,3 +90,18 @@ def confirm(token):
     else:
         flash('The confirmation link is invalid or has expired.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(
+        current_user.email,
+        'Confirm Your Account',
+        'auth/email/confirm',
+        user=current_user,
+        token=token,
+    )
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
