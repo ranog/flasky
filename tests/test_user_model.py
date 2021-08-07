3816010338ab72742b01f15eade105aeec9d2061
ranog <jprnogueira@yahoo.com.br>
2021-08-07 17:35:09 -0300

    Ex 9-9: unit tests for roles and permissions

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,28 +1,28 @@
 import time
 import unittest
 from app import db
-from app.models import User
+from app.models import AnonymousUser, Permission, User
 
 
 class UserModelTestCase(unittest.TestCase):
 
     def test_password_setter(self):
-        u = User(password = 'cat')
+        u = User(password='cat')
         self.assertTrue(u.password_hash is not None)
 
     def test_no_password_getter(self):
-        u = User(password = 'cat')
+        u = User(password='cat')
         with self.assertRaises(AttributeError):
             u.password
 
     def test_password_verification(self):
-        u = User(password = 'cat')
+        u = User(password='cat')
         self.assertTrue(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
 
     def test_password_salts_are_random(self):
-        u = User(password = 'cat')
-        u2 = User(password = 'cat')
+        u = User(password='cat')
+        u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
 
     def test_valid_confirmation_token(self):
@@ -41,7 +41,6 @@ class UserModelTestCase(unittest.TestCase):
         token = u1.generate_confirmation_token()
         self.assertFalse(u2.confirm(token))
 
-
     def test_expired_confirmation_token(self):
         u = User(password='cat')
         db.session.add(u)
@@ -49,3 +48,19 @@ class UserModelTestCase(unittest.TestCase):
         token = u.generate_confirmation_token(1)
         time.sleep(2)
         self.assertFalse(u.confirm(token))
+
+    def test_user_role(self):
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertTrue(u.can(Permission.COMMENT))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
+        self.assertFalse(u.can(Permission.COMMENT))
+        self.assertFalse(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
