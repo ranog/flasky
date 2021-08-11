c2f775de28e25c43a6324416b0a2f6d77d8e5b94
ranog <jprnogueira@yahoo.com.br>
2021-08-10 22:08:58 -0300

    Ex 10-5: user profile template

--- app/auth/views.py
+++ app/auth/views.py
@@ -54,7 +54,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(
-            email=form.email.data,
+            email=form.email.data.lower(),
             username=form.username.data,
             password=form.password.data,
         )
