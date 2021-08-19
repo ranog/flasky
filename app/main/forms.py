186d26d9bae0b18eac269c7f18be6abc8dd73410
ranog <jprnogueira@yahoo.com.br>
2021-08-18 23:30:26 -0300

    Ex 10-10: profile editing form for administrators

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,14 @@
+from app.models import Role, User
 from flask_wtf.form import FlaskForm
-from wtforms.fields.core import StringField
+from wtforms.fields.core import BooleanField, SelectField, StringField
 from wtforms.fields.simple import SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import (
+    DataRequired,
+    Email,
+    Length,
+    Regexp,
+    ValidationError,
+)
 
 
 class NameForm(FlaskForm):
@@ -14,3 +21,44 @@ class EditProfileForm(FlaskForm):
     location = StringField('Location', validators=[Length(0, 64)])
     about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
+
+
+class EditProfileAdminForm(FlaskForm):
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Length(1, 64), Email()],
+    )
+    username = StringField(
+        'Username',
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Regexp(
+                '^[A-Za-z][A-Za-z0-9_.]*$',
+                0,
+                'Usernames must have only letters, numbers, dots or underscores',  # NOQA
+            )
+        ]
+    )
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [
+            (role.id, role.name)
+            for role in Role.query.order_by(Role.name).all()
+        ]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and User.query.filter_by(email=field.data).first():  # NOQA
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and User.query.filter_by(username=field.data).first():  # NOQA
+            raise ValidationError('Username already in use.')
