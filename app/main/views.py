3ed59dec4db04b5007e747265e2b4d43c0bbdbec
ranog <jprnogueira@yahoo.com.br>
2021-08-21 00:46:00 -0300

    Ex 10-11: edit profile route for administrators

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 
-from .forms import EditProfileForm
-from ..models import User
+from .forms import EditProfileAdminForm, EditProfileForm
+from ..decorators import admin_required
+from ..models import Role, User
 
 from . import main
 from .. import db
@@ -35,3 +36,31 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit-profile/<int:id>', method=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(User)
+        db.session.commit()
+        flash('The profile has been updated.')
+        return redirect(url_for('user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profiel.html', form=form, user=user)
