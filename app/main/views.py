d1b14f0bc4a9c42e28128e18998918068779a4ae
ranog <jprnogueira@yahoo.com.br>
2021-10-15 09:00:12 -0300

    WIP

--- app/main/views.py
+++ app/main/views.py
@@ -1,20 +1,16 @@
 from flask import (
     render_template,
-    redirect,
-    url_for,
+    redirect, url_for,
     flash,
-    current_app,
     request,
+    current_app,
 )
 from flask_login import login_required, current_user
-from werkzeug.exceptions import abort
-
-from .forms import EditProfileAdminForm, EditProfileForm, PostForm
-from ..decorators import admin_required
-from ..models import Permission, Role, User, Post
-
 from . import main
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db
+from ..models import Permission, Role, User, Post
+from ..decorators import admin_required
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -23,33 +19,38 @@ def index():
     if current_user.can(Permission.WRITE) and form.validate_on_submit():
         post = Post(
             body=form.body.data,
-            author=current_user._get_current_object(),
+            author=current_user._get_current_object()
         )
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type=int)
     pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
-            page,
-            per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
-            erro_out=False,
-        )
+        page,
+        per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
     posts = pagination.items
     return render_template(
         'index.html',
         form=form,
         posts=posts,
-        pagination=pagination,
-    )
+        pagination=pagination)
 
 
 @main.route('/user/<username>')
 def user(username):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        abort(404)
-    posts = user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template('user.html', user=user, posts=posts)
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page,
+        per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template(
+        'user.html',
+        user=user,
+        posts=posts,
+        pagination=pagination)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
@@ -84,7 +85,7 @@ def edit_profile_admin(id):
         user.name = form.name.data
         user.location = form.location.data
         user.about_me = form.about_me.data
-        db.session.add(User)
+        db.session.add(user)
         db.session.commit()
         flash('The profile has been updated.')
         return redirect(url_for('.user', username=user.username))
@@ -95,4 +96,4 @@ def edit_profile_admin(id):
     form.name.data = user.name
     form.location.data = user.location
     form.about_me.data = user.about_me
-    return render_template('edit_profiel.html', form=form, user=user)
+    return render_template('edit_profile.html', form=form, user=user)
