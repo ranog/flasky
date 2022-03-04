92c521e55813ccd6bf12faa9d35e377253bf9cbd
ranog <jprnogueira@yahoo.com.br>
2022-03-03 21:38:37 -0300

    traditional way to set up the application routes

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
 def index():
     return '<h1>Hello World!<h1>'
+
+
+app.add_url_rule(rule='/', endpoint='index', view_func=index)
