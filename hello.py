0885be4ff3c3bbe8282eabe0277be2a7162bc7c4
ranog <jprnogueira@yahoo.com.br>
2022-03-04 14:05:34 -0300

    add route that has a dynamic component

--- hello.py
+++ hello.py
@@ -3,8 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
 def index():
     return '<h1>Hello World!<h1>'
 
 
-app.add_url_rule(rule='/', endpoint='index', view_func=index)
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hello, {name}!</h1>'
