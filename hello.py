a65de03c6d46e852543df63b14df9ec998f0268c
ranog <jprnogueira@yahoo.com.br>
2022-03-09 12:57:38 -0300

    add redirect function

--- hello.py
+++ hello.py
@@ -1,13 +1,11 @@
-from flask import Flask, make_response
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
+    return redirect('http://www.example.com')
 
 
 @app.route('/user/<name>')
