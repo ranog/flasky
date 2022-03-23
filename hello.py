f1a51bbb97b823d4dcd9962e1b27008a0c9e379b
ranog <jprnogueira@yahoo.com.br>
2022-03-23 13:17:41 -0300

    ex 4-4: handle a web form with GET and POST request methods

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -12,9 +11,19 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
@@ -30,8 +39,3 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name', validators=[DataRequired()])
-    submit = SubmitField('Submit')
